4480d73c5773215aa25036cd09c6f43b5046fe68
ilhamgs1427 <pp3583316@gmail.com>
2022-09-29 15:31:22 +0700

    commit pertama

---
+++ app/src/main/java/com/example/ilhamcollection/hal_utama.kt
@@ -0,0 +1,11 @@
+package com.example.ilhamcollection
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class hal_utama : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hal_utama)
+    }
+}
